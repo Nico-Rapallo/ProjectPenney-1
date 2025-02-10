593cce2b184c8f64350f5d79f6cc5b3a3a43e8f9
Smith <rdsmith@wm.edu>
2025-02-10 14:13:22 -0500

    updated README

--- src/helpers.py
+++ src/helpers.py
@@ -6,12 +6,11 @@ PATH_FIGURES = 'figures'
 
 def debugger_factory(show_args = True) -> Callable:
     def debugger(func: Callable) -> Callable:
-        def wrapper (*args, **kwargs):
+        def wrapper(*args, **kwargs):
             if show_args:
                 print(f'{func.__name__} was called with:')
                 print('Positional arguments:\n', args)
                 print('Keyword arguments:\n', kwargs)
-
             t0 = dt.now()
             results = func(*args, **kwargs)
             print(f'{func.__name__} ran for {dt.now() - t0}')
